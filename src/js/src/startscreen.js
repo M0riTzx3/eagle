fc9c952aa4973e471632a3d7f55fb8f11f07b8ca
dominik.mack <dominik@kathke-research.de>
2017-03-02 11:24:47 +0100

    Add Google Analytics and save Score.

--- src/js/src/startscreen.js
+++ src/js/src/startscreen.js
@@ -2,27 +2,34 @@ import trackEvent from "./googleAnalytics"
 import smafLoader from "./smafLoader"
 
 var selectedItem;
+var score = 0;
 
-    function init() {
+function init() {
     smafLoader();
     checkQuerys();
-        Smaf.on('action', menuControls);    
-    }
+    Smaf.on('action', menuControls);    
+}
 
-    function checkQuerys() {
+function checkQuerys() {
     var search = document.location.search;
-    if(search != '') {
+    if(search !== '') {
         setScore(getQueryParams(document.location.search).score);
     }
-    }
+}
 
-    function setScore(score) {
-    console.log(score);
-    $("#score").removeClass("invisible");
-    $("#score").html("<h1>You earned " + score + " Points!</h1>");
+function setScore(searchscore) {
+    if(searchscore !== undefined) {
+        score = searchscore;
+        trackEvent({
+				category: "game-end",
+				action: "Game ended with a score of " + score + " points."
+        });
+        $("#score").removeClass("invisible");
+        $("#score").html("<h1>You earned " + score + " Points!</h1>");
     }
+}
 
-    function menuControls(selection) {
+function menuControls(selection) {
     if(selection.keyCode === 38 || selection.type === 'UP') {
         $( "#exitgame" ).removeClass("selected");
         $( "#startgame" ).addClass("selected");
@@ -33,16 +40,17 @@ var selectedItem;
         selectedItem = 'exitgame';
     } else if (selection.keyCode === 13 || selection.type === 'ENTER') {
         if(selectedItem != null) {
-        if(selectedItem === 'startgame') {
-            window.location.href = "selectplayer.html"
-        } else {
-            window.location.href = "#"
-        }
+            if(selectedItem === 'startgame') {
+                window.location.href = "selectplayer.html"
+            } else {
+                window.location.href = "#"
+                Smaf.storage().setItem("highscore", this.score);
+            }
         }
     }
-    };
+}
 
-    function getQueryParams(qs) {
+function getQueryParams(qs) {
     qs = qs.split('+').join(' ');
 
     var params = {},
