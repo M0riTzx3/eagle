004fb112633ac84121d4190bbf24e89a7b1d67a3
Patrick Fey <patrick_fey@goodyear.com>
2017-03-02 14:07:59 +0100

    fixed deploy.bat

--- src/deploy.bat
+++ src/deploy.bat
@@ -1,10 +1,7 @@
-SET deploy_location=C:\Users\AA07511\tvapps\Eagle360RunnerShadow
-ECHO ON
-ECHO %deploy_location%
-ECHO HI
-xcopy /s js %deploy_location%\js /y/V
-xcopy /s/y images %deploy_location%\images /y/V
-xcopy /s/y styles %deploy_location%\styles /y/V
+SET deploy_location=C:\Users\AA07511\tvapps\Eagle360Runner
+xcopy /s js %deploy_location%\js /y /V
+xcopy /s images %deploy_location%\images /y /V
+xcopy /s styles %deploy_location%\styles /y /V
 xcopy appinfo.json %deploy_location% /y
 xcopy game.html %deploy_location% /y
 xcopy icon.png %deploy_location% /y
