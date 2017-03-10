403032a80631452edf66d2179b326c2563b529ce
Patrick Fey <patrick_fey@goodyear.com>
2017-03-10 12:57:08 +0100

    Removed unecessary pictures

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,7 @@ SET deploy_location=C:\Users\AA07511\tvapps\Eagle360Runner
 xcopy /s js %deploy_location%\js /y /V
 xcopy /s images %deploy_location%\images /y /V
 xcopy /s styles %deploy_location%\styles /y /V
+xcopy /s vendor %deploy_location%\vendor /y /V
 xcopy appinfo.json %deploy_location% /y
 xcopy game.html %deploy_location% /y
 xcopy icon.png %deploy_location% /y
